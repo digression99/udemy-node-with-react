9129308f6050dfbcbc3b57f711d4e75eccc568da
Pseudocoder Kim <pseudocoder@Pseudocoderui-MacBook-Pro.local>
2018-07-05 10:54:40 +0900

    github init

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const {
     PORT,
@@ -11,9 +13,15 @@ if (NODE_ENV !== 'production') {
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hli : 'there'});
-});
+// client id, client secret is in .env.
+passport.use(new GoogleStrategy());
+
+
+// app.get('/', (req, res) => {
+//     res.send({hli : 'there'});
+// });
+
+console.log('node env is : ', NODE_ENV);
 
 app.listen(PORT || 5000, () => {
     console.log('listening on port : ', PORT || 5000);
