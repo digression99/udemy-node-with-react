7b733ba4510bfd68e1a0bd976e36fd1a918496e4
Pseudocoder Kim <pseudocoder@Pseudocoderui-MacBook-Pro.local>
2018-08-11 19:04:06 +0900

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -29,7 +29,8 @@ passport.deserializeUser(async(id, done) => {
 passport.use(new GoogleStrategy({
     clientID : OAUTH_CLIENT_ID,
     clientSecret:OAUTH_CLIENT_SECRET,
-    callbackURL : `/auth/google/callback`
+    callbackURL : `/auth/google/callback`,
+    proxy : true
 }, async (accessToken, refreshToken, profile, done) => {
     // callback function.
     // console.log('access token : ', accessToken);
