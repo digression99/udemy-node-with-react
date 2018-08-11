8a3b5d82e0e962d93f07084633332dbda12db9fc
Pseudocoder Kim <pseudocoder@Pseudocoderui-MacBook-Pro.local>
2018-08-11 19:12:01 +0900

    proxy added

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
 const router = require('express').Router();
 
-router.get('/auth/google/callback', passport.authenticate('google'));
+router.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+    res.send(req.user);
+});
 
 router.get('/auth/google', passport.authenticate('google', {
     scope : ['profile', 'email']
