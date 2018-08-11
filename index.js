6e6ca01ddc38741b90b1cacb3acf3b169403c10f
Pseudocoder Kim <pseudocoder@Pseudocoderui-MacBook-Pro.local>
2018-08-11 18:39:43 +0900

    production ready

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 if (process.env.NODE_ENV !== 'production') {
+    console.log('development mode.');
     require('dotenv').config();
+} else if (process.env.NODE_ENV === 'production') {
+    console.log('production mode.');
 }
 
 const express = require('express');
